7fd8cbc5653989baa1aad6c7af579326bb29f7c4
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-01-10 21:30:47 +0800

    parent/child components

--- index.js
+++ index.js
@@ -229,14 +229,46 @@ Part 2:
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
+            <Header />
+            
+            <MainContent />
+            
+            <Footer />
+        </div>
+    )
+}
+
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+function MainContent() {
+    return (
+        <div>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
@@ -244,10 +276,15 @@ function Page() {
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
         </div>
+    )
+}
+
+function Footer() {
+    return(
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
     )
 }
 
